efcc56bc3894acb75bf9e641052fc223882471a6
Victor Nivet <victor@saumon.ninja>
2018-12-26 19:58:41 +0100

    add Config read from a config file

---
+++ src/config.rs
@@ -0,0 +1,40 @@
+use std::path::Path;
+use std::default::Default;
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
+pub struct Config {
+    pub ip: String,
+    pub port: u64,
+    pub data: String,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            ip: "127.0.0.1".into(),
+            port: 8080,
+            data: "data/".into(),
+        }
+    }
+}
+
+impl Config {
+    pub fn data_path(&self) -> &Path {
+        Path::new(&self.data)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use toml;
+
+    // test that the default config delivered is indeeed the same as Config::default()
+    #[test]
+    fn test_default_data() {
+        let default_config_toml = include_str!("../puizcloud.toml");
+        let config_from_toml: Config = toml::from_str(&default_config_toml).expect("Failed to parse TOML");
+        let default_config = Config::default();
+        assert_eq!(&default_config, &config_from_toml);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,44 @@
-extern crate actix_web;
+#[macro_use]
+extern crate serde_derive;
 use actix_web::{server, App, Responder, HttpResponse, fs::NamedFile};
+use toml;
 use std::{env, path};
 
+mod config;
+use self::config::Config;
+
+#[derive(Debug, Clone)]
+struct PuizcloudState {
+    config: Config,
+    full_data_path: path::PathBuf,
+}
+
+impl PuizcloudState {
+    pub fn new(config: Config) -> PuizcloudState {
+        let full_data_path = if config.data_path().is_absolute() {
+            config.data_path().to_owned()
+        }
+        else {
+            env::current_dir().expect("Unable to determine current directory")
+                .join(config.data_path())
+        };
+        PuizcloudState {
+            config,
+            full_data_path,
+        }
+    }
+    pub fn config(&self) -> &Config {
+        &self.config
+    }
+    pub fn full_data_path(&self) -> &path::Path {
+        &self.full_data_path
+    }
+}
+
+type HttpRequest = actix_web::HttpRequest<PuizcloudState>;
+
 struct EntrySubfolder {
-    subfolder_path: path::PathBuf,
+    given_path: path::PathBuf,
 }
 
 struct EntryFile {
@@ -11,16 +46,16 @@ struct EntryFile {
     file_size: u64,
 }
 
-fn directory_listing(dir_path: &path::Path) -> Result<(Vec<EntrySubfolder>, Vec<EntryFile>), actix_web::error::Error> {
+fn directory_listing(given_path: &path::Path, actual_path: &path::Path) -> Result<(Vec<EntrySubfolder>, Vec<EntryFile>), actix_web::error::Error> {
     let mut subfolders = Vec::new();
     let mut files = Vec::new();
-    for entry in dir_path.read_dir()? {
+    for entry in actual_path.read_dir()? {
         if let Ok(entry) = entry {
             let entry_path = entry.path();
-            if let Some(_) = entry_path.file_name() {
+            if let Some(entry_file_name) = entry_path.file_name() {
                 if entry_path.is_dir() {
                     subfolders.push(EntrySubfolder {
-                        subfolder_path: entry_path,
+                        given_path: given_path.join(entry_file_name),
                     })
                 }
                 else {
@@ -34,7 +69,7 @@ fn directory_listing(dir_path: &path::Path) -> Result<(Vec<EntrySubfolder>, Vec<
             }
         }
     }
-    subfolders.sort_unstable_by(|f1, f2| f1.subfolder_path.cmp(&f2.subfolder_path));
+    subfolders.sort_unstable_by(|f1, f2| f1.given_path.cmp(&f2.given_path));
     files.sort_unstable_by(|f1, f2| f1.file_path.cmp(&f2.file_path));
     Ok((subfolders, files))
 }
@@ -43,8 +78,8 @@ const PAGE_TITLE: &'static str = "Puizcloud";
 
 type WebResult = std::result::Result<actix_web::dev::AsyncResult<actix_web::HttpResponse>, actix_web::Error>;
 
-fn do_browse_directory(req: &actix_web::HttpRequest, given_path: &path::Path, actual_path: &path::Path) -> WebResult {
-    let (subfolders, files) = directory_listing(&actual_path)?;
+fn do_browse_directory(req: &HttpRequest, given_path: &path::Path, actual_path: &path::Path) -> WebResult {
+    let (subfolders, files) = directory_listing(given_path, actual_path)?;
     let current_path: String = 
         given_path.components()
             .filter_map(|p| 
@@ -93,7 +128,7 @@ fn do_browse_directory(req: &actix_web::HttpRequest, given_path: &path::Path, ac
         }
         // subfolders
         for subfolder in subfolders {
-            let path: &path::Path = &subfolder.subfolder_path;
+            let path: &path::Path = &subfolder.given_path;
             files_table.push_str(
                 &format!(r#"<tr><td><a href="{1}">{0}</a></td><td>--</td></tr>
                 "#,
@@ -145,22 +180,17 @@ fn file_not_found(file_path: &path::Path) -> actix_web::HttpResponse {
         .body(format!("HTTP Error 404 Not Found: {}", file_path.display()))
 }
 
-fn do_browse(req: &actix_web::HttpRequest) -> WebResult {
-    let given_path = path::PathBuf::from(req.match_info().get_decoded("tail").unwrap_or("".into()));
-    let current_dir = match env::current_dir() {
-        Ok(current_dir) => current_dir,
-        Err(_) => {
-            return file_not_found(path::Path::new("."))
-                .respond_to(&req);
-        },
-    };
+fn do_browse(req: &HttpRequest) -> WebResult {
+    let given_path_string = req.match_info().get_decoded("tail").unwrap_or("".into());
+    let given_path = path::Path::new(&given_path_string);
     if given_path.is_relative() {
-        if given_path.is_dir() {
-            do_browse_directory(req, &given_path, &current_dir.join(&given_path))
+        let actual_path = req.state().full_data_path().join(given_path);
+        if actual_path.is_dir() {
+            do_browse_directory(req, &given_path, &actual_path)
         }
         else {
-            if given_path.is_file() {
-                NamedFile::open(given_path)
+            if actual_path.is_file() {
+                NamedFile::open(&actual_path)
                     .expect("Failed to open named file")
                     .respond_to(&req)
                     .respond_to(&req)
@@ -168,7 +198,7 @@ fn do_browse(req: &actix_web::HttpRequest) -> WebResult {
             else {
                 // empty path: default folder
                 if given_path.components().next().is_none() {
-                    do_browse_directory(req, &path::Path::new(""), &current_dir)
+                    do_browse_directory(req, &path::Path::new(""), &actual_path)
                 }
                 else {
                     file_not_found(&given_path)
@@ -183,14 +213,26 @@ fn do_browse(req: &actix_web::HttpRequest) -> WebResult {
     }
 }
 
+fn read_config() -> Config {
+    use std::fs;
+    use toml;
+    let configuration_toml_file = path::Path::new("./puizcloud.toml");
+    fs::read(configuration_toml_file)
+        .ok()
+        .and_then(|v| toml::from_slice::<Config>(&v).ok())
+        .unwrap_or_else(|| Config::default())
+}
+
 fn main() {
-    let ip = "127.0.0.1";
-    let port = 8080;
-    println!("Listening on {}:{}", ip, port);
-    println!("Serving folder {}", env::current_dir().unwrap().display());
-    println!("To access the file server: http://{}:{}/browse/", ip, port);
+    let config = read_config();
+    let puizcloud_state = PuizcloudState::new(config);
+    let ip = puizcloud_state.config().ip.clone();
+    let port = puizcloud_state.config().port.clone();
+    println!("Listening on {}:{}", &ip, &port);
+    println!("To access the file server: http://{}:{}/browse/", &ip, &port);
+    println!("Serving folder {}", puizcloud_state.full_data_path().display());
     server::new(
-        || App::new()
+        move || App::with_state(puizcloud_state.clone())
             .resource("/browse/{tail:.*}", |r| {
                 r.name("browse");
                 r.get().f(do_browse);
